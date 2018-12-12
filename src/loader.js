bd7e9517bcf0a14a2e004639450e85c6061feed9
FalkZ <24669719+FalkZ@users.noreply.github.com>
2018-12-12 09:53:49 +0100

    fixed empty fields

--- src/loader.js
+++ src/loader.js
@@ -1,30 +1,30 @@
-import { Main } from './Main.js'
+import { Main } from './Main.js';
 //import { Gallery } from './Gallery.elm'
-import registerServiceWorker from './registerServiceWorker'
+import registerServiceWorker from './registerServiceWorker';
 //import imports from '../imports'
 
-import yaml from 'js-yaml'
+import yaml from 'js-yaml';
 
-import joker from './joker.svg'
+import joker from './joker.svg';
 
 //console.log(content)
 
-import './main.styl'
-import './header.styl'
-import './Gallery.styl'
+import './main.styl';
+import './header.styl';
+import './Gallery.styl';
 
-import $ from 'jquery'
+import $ from 'jquery';
 
 if (
-	window.location.hash.includes('#invite_token=') ||
-	window.location.hash.includes('#recovery_token=')
+  window.location.hash.includes('#invite_token=') ||
+  window.location.hash.includes('#recovery_token=')
 ) {
-	window.location = '/admin/' + window.location.hash
+  window.location = '/admin/' + window.location.hash;
 }
 
 //Main.embed(document.getElementById('root'))
 
-const app = Main.fullscreen()
+const app = Main.fullscreen();
 
 /*app.ports.check.subscribe(function(word) {
 	const suggestions = spellCheck(word)
@@ -33,61 +33,70 @@ const app = Main.fullscreen()
 
 //const { general, de, en } = imports.content
 
-const formatDate = (d) => {
-	let month = '' + (d.getMonth() + 1)
-	let day = '' + d.getDate()
-	let year = d.getFullYear()
+const formatDate = d => {
+  let month = '' + (d.getMonth() + 1);
+  let day = '' + d.getDate();
+  let year = d.getFullYear();
 
-	if (month.length < 2) month = '0' + month
-	if (day.length < 2) day = '0' + day
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
 
-	return [ year, month, day ].join('-')
-}
+  return [year, month, day].join('-');
+};
 
-const getNow = () => formatDate(new Date(Date.now()))
+const getNow = () => formatDate(new Date(Date.now()));
 
 fetch('content/deutsch.yml')
-	.then((response) => {
-		//console.log(response)
-		return response
-	})
-	.then((response) => response.text())
-	.then((text) => yaml.safeLoad(text) || {})
-	.then((content) =>
-		Object.assign(content, {
-			now: getNow(),
-			logo: joker,
-			images: content.images.map(({ alt, src }) => ({
-				alt: alt || '',
-				src: src.replace('/dist', '')
-			}))
-			//events: content.events.map((event) =>
-			//	Object.assign(event, { date: formatDate(event.date) })
-			//)
-		})
-	)
-	.then((content) => {
-		console.log(content)
-		return content
-	})
-	.then((content) => app.ports.content.send(content))
-	.catch(console.error)
+  .then(response => {
+    //console.log(response)
+    return response;
+  })
+  .then(response => response.text())
+  .then(text => yaml.safeLoad(text) || {})
+  .then(content =>
+    Object.assign(content, {
+      now: getNow(),
+      logo: joker,
+      images: content.images.map(({ alt, src }) => ({
+        alt: alt || '',
+        src: src.replace('/dist', '')
+      })),
+      events: content.events.map(event =>
+        Object.assign(
+          {
+            date: '2000-01-01',
+            eventName: '',
+            eventUrl: '',
+            locationName: '',
+            locationUrl: ''
+          },
+          event
+        )
+      )
+    })
+  )
+  .then(content => {
+    console.log(content);
+    return content;
+  })
+  .then(content => app.ports.content.send(content))
+  .catch(console.error);
 
-registerServiceWorker()
+registerServiceWorker();
 
 $(document).ready(function() {
-	$('a').click(function({ target: { hash } }) {
-		if (hash) {
-			try {
-				$('html, body').animate(
-					{
-						scrollTop: $(hash).offset().top - 200
-					},
-					600
-				)
-			} catch (error) {
-				console.warn('scroll target not found', error)
-			}
-		}
-	})
-})
+  $('a').click(function({ target: { hash } }) {
+    if (hash) {
+      try {
+        $('html, body').animate(
+          {
+            scrollTop: $(hash).offset().top - 200
+          },
+          600
+        );
+      } catch (error) {
+        console.warn('scroll target not found', error);
+      }
+    }
+  });
+});
